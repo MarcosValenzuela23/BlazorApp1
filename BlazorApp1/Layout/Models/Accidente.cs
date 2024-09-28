ea714063e44dcd41b70e821b68009f23a220bba0
MarcosjValenzuela <juniorvalenzuela742@gmail.com>
2024-09-27 23:51:56 -0400

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Layout/Models/Accidente.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Layout.Models
+{
+    public class Accidente
+    {
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public decimal CostoEconomico { get; set; }
+        public int NumeroMuertos { get; set; }
+        public int NumeroHeridos { get; set; }
+        public int CantidadVehiculos { get; set; }
+    }
+
+}
---
+++ BlazorApp1/Layout/Models/Services/AccidenteService.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp1.Layout.Models;
+using Blazored.LocalStorage;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class AccidenteService
+{
+    private readonly ILocalStorageService _localStorage;
+    private const string StorageKey = "accidentes";
+
+    public AccidenteService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    public async Task<List<Accidente>> GetAccidentesAsync()
+    {
+        return await _localStorage.GetItemAsync<List<Accidente>>(StorageKey) ?? new List<Accidente>();
+    }
+
+    public async Task SaveAccidenteAsync(Accidente accidente)
+    {
+        var accidentes = await GetAccidentesAsync();
+        accidentes.Add(accidente);
+        await _localStorage.SetItemAsync(StorageKey, accidentes);
+    }
+
+    public async Task DeleteAccidenteAsync(Accidente accidente)
+    {
+        var accidentes = await GetAccidentesAsync();
+        accidentes.Remove(accidente);
+        await _localStorage.SetItemAsync(StorageKey, accidentes);
+    }
+}
+
